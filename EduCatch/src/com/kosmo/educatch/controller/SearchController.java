d69bbcac9fa59ce7dfe9953474ac72fa5ea57006
son <son@LAPTOP-L29O49H6>
2020-07-29 17:29:31 +0900

    컨트롤러

--- EduCatch/src/com/kosmo/educatch/controller/SearchController.java
+++ EduCatch/src/com/kosmo/educatch/controller/SearchController.java
@@ -1,8 +1,12 @@
 package com.kosmo.educatch.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SearchController {
-
+	Logger log = Logger.getLogger(TestController.class);
+	public ModelAndView SearchList() {
+		return null;
+	}
 }
