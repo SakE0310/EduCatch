686e04937373d9dbf55fa3b279076a08b2b6eaa2
chemi <chemi@LAPTOP-FIH6T3D5>
2020-07-30 10:17:11 +0900

    merge

--- EduCatch/src/com/kosmo/educatch/controller/SearchController.java
+++ EduCatch/src/com/kosmo/educatch/controller/SearchController.java
@@ -2,11 +2,12 @@ package com.kosmo.educatch.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.portlet.ModelAndView;
 
 @Controller
 public class SearchController {
 	Logger log = Logger.getLogger(TestController.class);
-	public ModelAndView SearchList() {
+	public ModelAndView searchList() {
 		return null;
 	}
 }
