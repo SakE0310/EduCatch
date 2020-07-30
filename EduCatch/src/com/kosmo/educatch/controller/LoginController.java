4dddf206da99947c85fc775ac3ed723d72573f9e
kimki <kimki@KINAM>
2020-07-30 12:09:14 +0900

    Login 링크 완료

--- EduCatch/src/com/kosmo/educatch/controller/LoginController.java
+++ EduCatch/src/com/kosmo/educatch/controller/LoginController.java
@@ -1,12 +1,32 @@
 package com.kosmo.educatch.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
+	private static String CONTEXT_PATH="login";
+	private static Logger log = Logger.getLogger(LoginController.class);
+	
+	//private NoticeService noticeService;
 	
 	
+	//공지사항 게시판 목록 조회
+	@RequestMapping("/login.ec")
+	public ModelAndView listNotice( ) {
+		log.info("NoticeController listNotice 시작 >>>");
 
-
-
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName(CONTEXT_PATH+"/login_insert");
+		
+		log.info("LoginController login 끝 >>>");
+		return mav;
+	}
 }
+
+	
+
+
+
