110a09a3940c1ac5c08e63c81d2795fde68fb290
ekfri <ekfri@DESKTOP-GHOQ9JG>
2020-07-30 10:13:17 +0900

    자유게시판 컨트롤러

--- EduCatch/src/com/kosmo/educatch/controller/SearchController.java
+++ EduCatch/src/com/kosmo/educatch/controller/SearchController.java
@@ -2,7 +2,6 @@ package com.kosmo.educatch.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SearchController {
