f10f8a83c84552d9180b95b1428e58b3c775d965
chemi <chemi@LAPTOP-FIH6T3D5>
2020-07-30 11:19:24 +0900

    review11

--- EduCatch/src/com/kosmo/educatch/controller/ReviewController.java
+++ EduCatch/src/com/kosmo/educatch/controller/ReviewController.java
@@ -1,10 +1,18 @@
 package com.kosmo.educatch.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.portlet.ModelAndView;
+
+import com.kosmo.educatch.service.ReviewService;
+import com.kosmo.educatch.vo.ReviewVO;
 
 
 @Controller
@@ -14,10 +22,20 @@ public class ReviewController {
 	
 	private static final String CONTEXT_PATH="reviewboard";
 	
-
+	@Autowired
+	private ReviewService reviewService;
 
 	
-	//@RequestMapping("/listReview.ec")
+	@RequestMapping("/listReview.ec")
+	public ModelAndView listReview(@ModelAttribute ReviewVO param,
+									HttpServletRequest request) {
+		
+		log.info("ReviewController listReview >>> 호출성공");
+		
+		List<ReviewVO>
+	}
+	
+	
 
 	
 }
---
+++ EduCatch/src/com/kosmo/educatch/vo/ReviewVO.java
@@ -0,0 +1,136 @@
+package com.kosmo.educatch.vo;
+
+public class ReviewVO {
+	
+	private String rbno;
+	private String rbsubject;
+	private String rbname;
+	private String rbimg;
+	private String rbcontent;
+	private String academyano;
+	private String membermno;
+	private String rbgrade;
+	private String rbdeleteyn;
+	private String rbinsertdate;
+	private String rbupdatedate;
+	
+	
+	public ReviewVO() {
+		
+	}
+
+
+	public String getRbno() {
+		return rbno;
+	}
+
+
+	public void setRbno(String rbno) {
+		this.rbno = rbno;
+	}
+
+
+	public String getRbsubject() {
+		return rbsubject;
+	}
+
+
+	public void setRbsubject(String rbsubject) {
+		this.rbsubject = rbsubject;
+	}
+
+
+	public String getRbname() {
+		return rbname;
+	}
+
+
+	public void setRbname(String rbname) {
+		this.rbname = rbname;
+	}
+
+
+	public String getRbimg() {
+		return rbimg;
+	}
+
+
+	public void setRbimg(String rbimg) {
+		this.rbimg = rbimg;
+	}
+
+
+	public String getRbcontent() {
+		return rbcontent;
+	}
+
+
+	public void setRbcontent(String rbcontent) {
+		this.rbcontent = rbcontent;
+	}
+
+
+	public String getAcademyano() {
+		return academyano;
+	}
+
+
+	public void setAcademyano(String academyano) {
+		this.academyano = academyano;
+	}
+
+
+	public String getMembermno() {
+		return membermno;
+	}
+
+
+	public void setMembermno(String membermno) {
+		this.membermno = membermno;
+	}
+
+
+	public String getRbgrade() {
+		return rbgrade;
+	}
+
+
+	public void setRbgrade(String rbgrade) {
+		this.rbgrade = rbgrade;
+	}
+
+
+	public String getRbdeleteyn() {
+		return rbdeleteyn;
+	}
+
+
+	public void setRbdeleteyn(String rbdeleteyn) {
+		this.rbdeleteyn = rbdeleteyn;
+	}
+
+
+	public String getRbinsertdate() {
+		return rbinsertdate;
+	}
+
+
+	public void setRbinsertdate(String rbinsertdate) {
+		this.rbinsertdate = rbinsertdate;
+	}
+
+
+	public String getRbupdatedate() {
+		return rbupdatedate;
+	}
+
+
+	public void setRbupdatedate(String rbupdatedate) {
+		this.rbupdatedate = rbupdatedate;
+	}
+	
+	
+	
+	
+
+}
