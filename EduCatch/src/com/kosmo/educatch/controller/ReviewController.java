d5e81cc344fc61488a4c2b2d6dbf9257286246fd
chemi <chemi@LAPTOP-FIH6T3D5>
2020-07-30 10:55:03 +0900

    rrr

--- EduCatch/src/com/kosmo/educatch/controller/ReviewController.java
+++ EduCatch/src/com/kosmo/educatch/controller/ReviewController.java
@@ -3,6 +3,8 @@ package com.kosmo.educatch.controller;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.ModelAndView;
 
 
 @Controller
@@ -12,10 +14,10 @@ public class ReviewController {
 	
 	private static final String CONTEXT_PATH="reviewboard";
 	
-	@Autowired
-	private ReviewService reviewService;
+
+
 	
-	@Requ
+	//@RequestMapping("/listReview.ec")
 
 	
 }
