441041e09cb03101b537f7a14bf5520f468573cf
son <son@LAPTOP-L29O49H6>
2020-07-30 11:32:21 +0900

    search

---
+++ EduCatch/src/com/kosmo/educatch/dao/SearchMapperImpl.java
@@ -0,0 +1,7 @@
+package com.kosmo.educatch.dao;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+public class SearchMapperImpl extends SqlSessionDaoSupport implements SearchMapper{
+
+}
