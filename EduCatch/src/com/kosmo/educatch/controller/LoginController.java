664537990894105b9d9fd2a51024625f31fb2ddb
kimki <kimki@KINAM>
2020-07-30 10:30:30 +0900

    ll

--- EduCatch/src/com/kosmo/educatch/controller/LoginController.java
+++ EduCatch/src/com/kosmo/educatch/controller/LoginController.java
@@ -1,10 +1,12 @@
 package com.kosmo.educatch.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class LoginController {
+	
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
 
 }
