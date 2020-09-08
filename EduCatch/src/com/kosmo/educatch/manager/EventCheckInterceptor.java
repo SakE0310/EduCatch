f302a4ccff850f11bf55ec1dd5a2c04b838275ad
hyemi00 <hmmi2580@gmail.com>
2020-09-08 14:35:27 +0900

    게시판 인터셉터

---
+++ EduCatch/src/com/kosmo/educatch/manager/EventCheckInterceptor.java
@@ -0,0 +1,44 @@
+package com.kosmo.educatch.manager;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.kosmo.educatch.controller.EventController;
+import com.kosmo.educatch.vo.MemberVO;
+
+public class EventCheckInterceptor  extends HandlerInterceptorAdapter{
+private Logger log = LoggerManager.getInstance().getLogger(EventController.class);
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		log.info("Event Check >>> ");
+		MemberVO mvo = (MemberVO)request.getSession().getAttribute("user");
+		String mauth = null;
+		String id = null;
+		if(mvo != null) {
+			id = mvo.getMid();
+			mauth = mvo.getMauth();	
+		}
+		log.info("mauth : "+mauth);
+		log.info("id : "+id);
+		if( id==null || (mauth.equals("1") || mauth.equals("2"))) {
+			//관리자모드가 아닐때 
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter pw = response.getWriter();
+			pw.println("<script type='text/javascript' charset='utf-8'> ");
+			pw.println("alert('관리자 기능입니다. 새로운 접속을 시도해주세요');");
+			pw.println("location.href='http://localhost:8088/EduCatch/index.jsp'");
+			pw.println("</script>");
+			pw.flush();
+			return false;
+		}
+		return super.preHandle(request, response, handler);
+	}
+}
